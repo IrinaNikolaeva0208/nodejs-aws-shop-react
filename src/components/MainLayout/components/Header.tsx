ab96975790066398d958920c6eea9bc425a7d662
IrinaNikolaeva <taffy.grr@gmail.com>
2026-04-24 15:46:31 +0300

    change header color

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="relative">
+    <AppBar position="relative" sx={{ bgcolor: "orange" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
